946cc97c84c652eef7afddb1fcfee46c026192c8
Vanga Kiran Kumar Reddy <kirankumarrrreddy@gmail.com>
2022-11-10 00:12:46 +0530

    Done with Counter Lesson Simple State.

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./screens/HomeScreen";
 import ComponentsScreen from './screens/ComponentsScreen';
 import ListScreen from './screens/ListScreen';
+import ImageScreen from './screens/ImageScreen';
+import CounterScreen from './screens/CounterScreen';
 
 //Decides which content to be displayed on Screen
 const navigator = createStackNavigator(
@@ -10,6 +12,8 @@ const navigator = createStackNavigator(
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen
   },
   {
     initialRouteName: "Home",
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,26 +3,38 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import ComponentsScreen from "./ComponentsScreen";
 
 const HomeScreen = (props) => {
-// console.log('props :', props.navigation);
+  // console.log('props :', props.navigation);
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
-     <Button 
-      onPress={()=>{
-        console.log("Button Pressed");
-        props.navigation.navigate('Components')
-      }}
-     title="Go to CompoentScreen" />
-     <TouchableOpacity
-      onPress={()=>{
-        console.log("TouchableOpacity Pressed");
-        props.navigation.navigate('List')
-      }}
-     >
-       <Text style={styles.text}>Go to List Demo 1</Text>
-       <Text style={styles.text}>Go to List Demo 2</Text>
-       <Text style={styles.text}>Go to List Demo 3</Text>
-     </TouchableOpacity>
+      <Button
+        onPress={() => {
+          console.log("Button Pressed");
+          props.navigation.navigate('Components')
+        }}
+        title="Go to CompoentScreen" />
+      <Button
+        onPress={() => {
+          console.log("Button Pressed");
+          props.navigation.navigate('Image')
+        }}
+        title="Go to Image Screen" />
+      <Button
+        onPress={() => {
+          console.log("Button Pressed");
+          props.navigation.navigate('Counter')
+        }}
+        title="Go to Counter Screen" />
+      <TouchableOpacity
+        onPress={() => {
+          console.log("TouchableOpacity Pressed");
+          props.navigation.navigate('List')
+        }}
+      >
+        <Text style={styles.text}>Go to List Demo 1</Text>
+        <Text style={styles.text}>Go to List Demo 2</Text>
+        <Text style={styles.text}>Go to List Demo 3</Text>
+      </TouchableOpacity>
     </View>
   );
 };
