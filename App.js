3a8d96c92d522981ebbb3b4d5088cc3a0bef4a24
Vanga Kiran Kumar Reddy <kirankumarrrreddy@gmail.com>
2022-11-10 20:33:36 +0530

    Blog init

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from "./screens/HomeScreen";
-import ComponentsScreen from './screens/ComponentsScreen';
-import ListScreen from './screens/ListScreen';
-import ImageScreen from './screens/ImageScreen';
-import CounterScreen from './screens/CounterScreen';
+import IndexScreen from './src/screens/IndexScreen';
+
 
 //Decides which content to be displayed on Screen
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Components: ComponentsScreen,
-    List: ListScreen,
-    Image: ImageScreen,
-    Counter: CounterScreen
+    Home: IndexScreen,
   },
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "App"
+      title: "Blogs"
     }
   }
 );
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text>IndexScreen </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({});
+
+export default IndexScreen
