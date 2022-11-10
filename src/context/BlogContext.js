8c4106c807744b0b94a8803c470e639a68468929
Vanga Kiran Kumar Reddy <kirankumarrrreddy@gmail.com>
2022-11-10 21:00:40 +0530

    Simple Blog Add

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return (
-        <BlogContext.Provider value={5}>
-            {children}
-        </BlogContext.Provider>
-    )
-}
+  const [blogPosts, setBlogPosts] = useState([
+    {
+      title: 'Blog Post 1',
+    },
+    {
+      title: 'Blog Post 2',
+    },
+  ]);
 
-export default BlogContext
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post ${blogPosts.length + 1}` },
+    ]);
+  };
 
+  return (
+    <BlogContext.Provider
+      value={{
+        data: blogPosts,
+        addBlogPost,
+      }}
+    >
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,29 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
-
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
 
-    const value = useContext(BlogContext);
-
-    return (
-        <View>
-            <Text>IndexScreen -- {value}</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>IndexScreen </Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
 
 const style = StyleSheet.create({});
 
-export default IndexScreen
+export default IndexScreen;
